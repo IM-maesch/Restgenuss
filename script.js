9f4c0da10d0fd5d8fa0db031ade9d7c09914d269
eleenaberger <119845756+eleenaberger@users.noreply.github.com>
2023-10-09 13:20:56 +0200

    Lea Update

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ console.log("00 JavaScript verbunden")
 
 // 1. **Alle Filme abrufen**: Hole alle Spalten aller Filme aus der Tabelle `movies`.
 
-async function selectAllMovies() {
-    const { data, error } = await supa.from("movies").select();
+async function selectAllCategories() {
+    const { data, error } = await supa.from("Kategorien").select();
   
     return data;
   }
 
-console.log('Alle Filme in der DB: ', selectAllMovies());
+console.log(selectAllCategories());
