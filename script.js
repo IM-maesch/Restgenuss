83f928e58bb4be4c3e0b3e1b2f3da0f746f02870
Bellielle <119850107+Bellielle@users.noreply.github.com>
2023-10-09 15:11:09 +0200

    optisch Code beschönigt + Funktion aufgerufen showRezepte

--- script.js
+++ script.js
@@ -5,47 +5,49 @@ console.log("00 JavaScript verbunden")
 // 1. **Alle Filme abrufen**: Hole alle Spalten aller Filme aus der Tabelle `movies`.
 
 async function showCategories() {
-    const { data, error } = await supa.from("kategorien").select();
-    if(data)
-    {
-      let container= document.querySelector("#kategorien");
-data.forEach(kategorie=>{
- console.log(kategorie); 
-  let output=`
-  <div class="box">
-              <h2>${kategorie.name}</h2>
-              <p>Content for Box 1</p>
-              <button onclick="handleClick(1)">Button 1</button>
-            </div>
-  `;
-  container.innerHTML+=output;
-
-})
-      
-    }}
+  const { data, error } = await supa.from("kategorien").select();
+  if(data)
+  {
+    let container= document.querySelector("#kategorien");
+    data.forEach(kategorie=>{
+    console.log(kategorie); 
+    let output=`
+      <div class="box">
+        <h2>${kategorie.name}</h2>
+        <p>Content for Box 1</p>
+        <button onclick="handleClick(1)">${kategorie.name}</button>
+      </div>
+      `;
+    container.innerHTML+=output;
+    })   
+  }
+};
   
 
-    async function showRezepte() {
-      const { data, error } = await supa.from("rezepte").select();
-      if(data)
-      {
-        let container= document.querySelector("#rezeptVorschlag");
-  data.forEach(rezepte=>{
-   console.log(rezepte); 
+async function showRezepte() {
+  const { data, error } = await supa.from("rezepte").select();
+  if(data)
+  {
+    let container= document.querySelector("#rezeptVorschlag");
+    data.forEach(rezepte=>{
+    console.log(rezepte); 
     let output=`
-    <div class="box">
-                <h2>${rezepte.rezeptname}</h2>
-                <p>Content for Box 1</p>
-                <button onclick="handleClick(1)">Button 1</button>
-              </div>
+      <div class="box">
+        <h2>${rezepte.rezeptname}</h2>
+        <p>Content for Box 1</p>
+        <button onclick="handleClick(1)">Button 1</button>
+      </div>
     `;
     container.innerHTML+=output;
+    }) 
+  }
+}
+
+
   
-  })
-        
-      }}
-  
-    return data;
-  
+/*
+return data;
+*/
 
 console.log(showCategories());
+console.log(showRezepte());
