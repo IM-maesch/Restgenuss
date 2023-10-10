d497948e72d13d5a189b1abd8992d784e1fcaeb5
Bellielle <119850107+Bellielle@users.noreply.github.com>
2023-10-10 11:41:15 +0200

    js

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ console.log("00 JavaScript verbunden")
 //Zeigt alle Kategorien an, soll beim Laden der Startseite ausgeführt werden
 async function showCategories() {
   const { data, error } = await supa.from("kategorien").select();
-  if(data)
-  {
-    let container= document.querySelector("#kategorien");
-    data.forEach(kategorie=>{
-    console.log(kategorie); 
-    let output=`
-      <button class="box kategorie-button" id="Kategorie-id-${kategorie.id}">
-        <h2>${kategorie.name}</h2>
-      </button>
+  if (data) {
+    let container = document.querySelector("#kategorien");
+    data.forEach(kategorie => {
+      let output = `
+        <button class="box kategorie-button" id="Kategorie-id-${kategorie.id}">
+          <h2>${kategorie.name}</h2>
+        </button>
       `;
-    container.innerHTML+=output;
-    })   
+      container.innerHTML += output;
+      
+      // Füge einen Event-Listener hinzu
+      const button = document.querySelector(`#Kategorie-id-${kategorie.id}`);
+      button.addEventListener("click", () => {
+        console.log(`Angeklicktes Element ID: ${kategorie.id}`);
+      });
+    });
   }
-};
+}
+
 
 //Zeigt nach dem Ausführen alle Rezepte von jeder Kategorie an
 async function showRezepte() {
