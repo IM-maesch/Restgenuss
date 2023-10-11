aaf50df5f2c75553023591a0a34932210b315f4c
WIP-maesch <119851162+WIP-maesch@users.noreply.github.com>
2023-10-11 10:21:27 +0200

    Update script.js
    Funktionen bereinigt, Funktion erstellt zum Rezepte öffnen mit entsprechender ID in der URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { supa } from "/supabase.js";
+/*import { supa } from "/supabase.js";
 
 console.log("00 JavaScript verbunden")
 
@@ -27,13 +27,13 @@ document.querySelector("#kategorien").addEventListener("click", function(event) 
   const clickedButton = event.target;
   if (clickedButton.classList.contains("kategorie-button")) {
     const kategorie_id = clickedButton.id.split("-")[2];
-    // Call a function to display recipes based on kategorie_id
+    // Call a function to display recipe-buttons based on kategorie_id
     displayRecipesByCategory(kategorie_id);
     console.log(kategorie_id);
   }
 });
 
-// Function to display recipes for a specific category
+// Buttons mit den Rezepttiteln anzeigen, die in der angeklickten Kategorie sind
 async function displayRecipesByCategory(kategorie_id) {
   const { data, error } = await supa
     .from("rezepte")
@@ -42,7 +42,7 @@ async function displayRecipesByCategory(kategorie_id) {
   
   if (data) {
     let container = document.querySelector("#rezeptVorschlag");
-    container.innerHTML = ""; // Clear previous content
+    container.innerHTML = ""; // Vorherigen Inhalt löschen
     data.forEach(rezepte => {
       let output = `
         <button class="box">
@@ -54,24 +54,91 @@ async function displayRecipesByCategory(kategorie_id) {
   }
 }
 
+//-------------------------------------------------------
+//Code zum öffnen der Rezeptseite
 
-
-
-
-//Beim Klick auf die Kategorie sollen alle Rezepte der Kategorie angezeigt werden.
-//Javascript holt die ID des Buttons in HTML
-//document.getElementById("demo").onclick = function() {myFunction()};
-
-function myFunction() {
-  //document.getElementById("rezeptVorschlag").innerHTML = ${kategorie_id}"; //Hier sollten nun alle Rezepte der jeweiligen Kategorie angezeigt werden
-
+// Function to redirect to the recipe page based on the rezepte.id
+function viewRecipe(event) {
+  const clickedButton = event.target;
+  const recipeId = clickedButton.getAttribute('data-recipe-id');
+  if (recipeId) {
+      window.location.href = `rezept.html?id=${recipeId}`;
+  }
 }
-/*
-return data;
-*/
+
+// Event listener to handle button clicks
+document.addEventListener('click', function (event) {
+  if (event.target && event.target.tagName === 'BUTTON') {
+      viewRecipe(event);
+  }
+});
+
+
+//-----------------------------------------------------------
 
 
 //Aufruf der beiden weiter oben definierten Funktionen
 console.log(showCategories());
 console.log(showRezepte());
 
+*/
+
+
+import { supa } from "/supabase.js";
+
+console.log("00 JavaScript verbunden");
+
+// Zeigt alle Kategorien an, soll beim Laden der Startseite ausgeführt werden
+async function showCategories() {
+  const { data, error } = await supa.from("kategorien").select();
+  if (data) {
+    console.log(data);
+    let container = document.querySelector("#kategorien");
+    data.forEach(kategorie => {
+      let button = document.createElement("button");
+      button.className = "box kategorie-button";
+      button.id = `Kategorie-id-${kategorie.id}`;
+      button.innerHTML = `<h2>${kategorie.name}</h2>`;
+
+      // Add a click event listener for each category button
+      button.addEventListener("click", () => {
+        const kategorie_id = kategorie.id;
+        displayRecipesByCategory(kategorie_id);
+        console.log(kategorie_id);
+      });
+
+      container.appendChild(button);
+    });
+  }
+}
+
+// Buttons mit den Rezepttiteln anzeigen, die in der angeklickten Kategorie sind
+async function displayRecipesByCategory(kategorie_id) {
+  const { data, error } = await supa.from("rezepte").select().eq("kategorie_id", kategorie_id);
+
+  if (data) {
+    let container = document.querySelector("#rezeptVorschlag");
+    container.innerHTML = ""; // Vorherigen Inhalt löschen
+    data.forEach(rezepte => {
+      let button = document.createElement("button");
+      button.className = "box";
+      button.setAttribute("data-recipe-id", rezepte.id);
+      button.innerHTML = `<h2>${rezepte.rezeptname}</h2>`;
+
+      // Add a click event listener for each recipe button
+      button.addEventListener("click", () => {
+        viewRecipe(rezepte.id);
+      });
+
+      container.appendChild(button);
+    });
+  }
+}
+
+// Function to redirect to the recipe page based on the rezepte.id
+function viewRecipe(recipeId) {
+  window.location.href = `rezept.html?id=${recipeId}`;
+}
+
+// Call the showCategories function when the page loads
+document.addEventListener("DOMContentLoaded", showCategories);
