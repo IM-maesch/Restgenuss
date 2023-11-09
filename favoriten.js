9eef15367ae8661398b90646737a7be24871f67c
Bellielle <119850107+Bellielle@users.noreply.github.com>
2023-11-09 20:54:56 +0100

    Favoriten button

--- favoriten.js
+++ favoriten.js
@@ -82,7 +82,7 @@ if (user) {
 }
 
 fav.addEventListener("click", addToFavorites);
-
+*/
 
 
 /*
@@ -190,6 +190,61 @@ if (!user) {
 } else {
   document.getElementById('userStatus').textContent = `Authenticated as: ${user.id}`;
 }
+
+const fav = document.getElementById("favoritenButton");
+
+if(fav){
+  fav.addEventListener('click', function () {
+        // Wechsel zum nächsten Bild
+        currentIndex = (currentIndex + 1) % bilder.length;
+        fav.src = bilder[currentIndex]; 
+    }); }
+
+    async function addToFavorites() {
+      const user = supa.auth.user();
+      console.log("Benutzer-ID:", user.id)    
+
+      console.log("Benutzer-ID:", user.id);
+      const urlParams = new URLSearchParams(window.location.search);
+      const recipeId = urlParams.get("id");
+      console.log("Rezept-ID:", recipeId);
+      
+      // Füge das Rezept in die relation_fav_rezept Liste hinzu
+      const { data, error } = await supa
+      .from("user_faved_rezept")
+      .insert([
+        { user_id: user.id, rezept_id: recipeId }
+      ]);
+    
+      if (error) {
+        console.error("Fehler beim Hinzufügen des Rezepts zu den Favoriten:", error);
+      } else {
+        alert("Rezept wurde zu den Favoriten hinzugefügt!");
+      }
+    }
+
+            // Daten von Supabase abrufen um in favoriten.html zu implementieren
+        
+if (user) {
+  const { data, error } = await supa
+  .from('user_faved_rezept')
+  .select('rezept_id')
+  .eq('user_id', user.id)
+  
+  if (error) {
+    console.error('Fehler beim Abrufen der favorisierten Rezepte:', error);
+  } else {
+    // Hier kannst du mit den abgerufenen Daten arbeiten (z. B. sie auf deiner Webseite anzeigen)
+    console.log('Favorisierte Rezepte:', data);
+    // Zeige die Rezepte auf deiner Webseite an
+  }
+}
+
+    // Annahme: data enthält die abgerufenen Rezeptdaten
+    const rezeptContainer = document.getElementById('rezeptContainer');
+   
+  fav.addEventListener("click", addToFavorites);
+
 
 // Funktion, um zur Rezeptseite zu gelangen
 function rezeptAufrufen(recipeId) {
